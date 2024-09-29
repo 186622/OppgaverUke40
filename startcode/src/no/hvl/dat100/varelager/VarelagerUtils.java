20e1a53e3d30fbb3265bfb61db69934b5102c070
HanssenGLRP <66432612+186622@users.noreply.github.com>
2024-09-29 12:47:18 +0200

    B2) b) Fullfør metoder UTILS

--- startcode/src/no/hvl/dat100/varelager/VarelagerUtils.java
+++ startcode/src/no/hvl/dat100/varelager/VarelagerUtils.java
@@ -3,26 +3,43 @@ package no.hvl.dat100.varelager;
 public class VarelagerUtils {
 
 	public static Vare finnBilligste(Vare[] varer) {
+		Vare billigste = varer[0];
 		
-		throw new TODO("finnBilligste");
-
+		for (int i = 0; i < varer.length; i++) {
+			if (varer[i].getPris() < billigste.getPris()) {
+				billigste = varer[i];
+			}
+		}
+		return billigste;
+	
 	}
 	
 	public static double totalPris(Vare[] varer) {		
-		
-		throw new TODO("totalPris");
-
+		double sum = 0;
+		for (int i = 0; i < varer.length; i++) {
+			sum = varer[i].getPris();
+		}
+		return sum;
 	}
 	
 	public static int[] finnVarenr(Vare[] varer) {
-				
-		throw new TODO("finnVarenr");
+		int[] vareNummer = new int[varer.length];
+		
+		for (int i = 0; i < varer.length;i++) {
+			vareNummer[i] = varer[i].getVarenr();
+		}
+		return vareNummer;
 
 	}
 	
 	public static double[] finnPrisDifferanser(Vare[] varer) {
 				
-		throw new TODO("finnPrisDifferanser");
+		double[] differanseTabell = new double[varer.length-1];
+		
+		for (int i = 0; i < varer.length -1;i++){
+			differanseTabell[i] = varer[i+1].getPris() - varer[i].getPris();
+		}
+		return differanseTabell;
 
 	}
 }
